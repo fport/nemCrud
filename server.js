b2645a2a06e7c4109a625cd36d27f8786188b111
FurkanPortakal <furkanportakalx@gmail.com>
2020-12-25 16:18:01 +0300

    update

--- server.js
+++ server.js
@@ -1,33 +1,34 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const morgan = require("morgan");
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
 const bodyparser = require("body-parser");
-const path = require("path");
+const path = require('path');
+
+const connectDB = require('./server/database/connection');
 
 const app = express();
 
-dotenv.config({ path: "config.env" });
-const PORT = process.env.PORT || 8080;
- 
-//log request
-app.use(morgan("tiny"));
-//parse request to body-parser
-app.use(bodyparser.urlencoded({ extended: true }));
+dotenv.config( { path : 'config.env'} )
+const PORT = process.env.PORT || 8080
+// log requests
+app.use(morgan('tiny'));
 
-//set view engine
-app.set("view engine", "ejs");
-// app.set("views",path.resolve(__dirname,"views/ejs"))
+// mongodb connection
+connectDB();
 
-//load assets
-app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
-app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
-app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
-//css/style.css
+// parse request to body-parser
+app.use(bodyparser.urlencoded({ extended : true}))
 
-app.get("/", (req, res) => {
-  res.render("index")
-});
+// set view engine
+app.set("view engine", "ejs")
+//app.set("views", path.resolve(__dirname, "views/ejs"))
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// load assets
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
+
+// load routers
+app.use('/', require('./server/routes/router'))
+
+app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
