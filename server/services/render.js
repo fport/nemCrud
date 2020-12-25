00a70831a10f8b36a5ab0865a432cca45f8d4261
Kayra <kayrabtuncer@gmail.com>
2020-12-25 21:21:04 +0300

    edit link

--- server/services/render.js
+++ server/services/render.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 exports.homeRoutes = (req, res) => {
     // Make a get request to /api/users
-    axios.get('http://localhost:8080/api/users')
+    axios.get('http://localhost:3000/api/users')
         .then((response) => {
             res.render('index', { users : response.data });
         })
@@ -19,7 +19,7 @@ exports.add_user = (req, res) =>{
 }
 
 exports.update_user = (req, res) =>{
-    axios.get('http://localhost:8080/api/users', { params : { id : req.query.id }})
+    axios.get('http://localhost:3000/api/users', { params : { id : req.query.id }})
         .then(function(userdata){
             res.render("update_user", { user : userdata.data})
         })
